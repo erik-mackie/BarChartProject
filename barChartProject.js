50395b7f82ff75f1da11b012f9e6d56341428a9c
Erik Mackie <erik@erik.workStation>
2018-08-19 12:13:29 -0700

    Rough page controls added

--- barChartProject.js
+++ barChartProject.js
@@ -1,8 +1,31 @@
-console.log("first commit")
+//Title Color controls
+//Title Color controls
+/*const titleInput = document.querySelector(".titleInput");
+const titleSize = document.querySelector(".titleSize");
+const titleColour = document.querySelector(".titleColour");
+const titleAdd = document.querySelector('#titleAdd');
+const graphTitle = document.getElementById("graphTitle")*/
+
+//change Graph Title text
+$( "input" )
+  .keyup(function() {
+    var titleValue = $( this ).val();
+    $( "#graphTitle" ).text( titleValue );
+  })
+  .keyup();
 
 
 
 
 
 
-// time on project 3h 2h
+
+
+
+/*drawBarChart(data, options, element);*/
+
+
+
+
+
+// time on project 3h 2h  3h 3h 2.5h 3h 2h
